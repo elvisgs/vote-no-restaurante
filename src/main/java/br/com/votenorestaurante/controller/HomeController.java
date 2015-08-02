5bab70f92baceb17807c28308e6208d2a5075cdb
Elvis Luciano <elvis.gs@gmail.com>
2015-08-02 20:07:05 -0300

    Configuração do vRaptor

---
+++ src/main/java/br/com/votenorestaurante/controller/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.votenorestaurante.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+
+@Controller
+public class HomeController {
+
+    @Get("/")
+    public void index() {
+
+    }
+}
